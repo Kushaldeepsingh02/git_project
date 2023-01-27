bada392962b685c9010c5eca0deef41e8a8412df
j22ku <j22ku@10.190.2.15>
2023-01-27 17:24:26 -0500

    first commit

--- src/git_project/test.java
+++ src/git_project/test.java
@@ -4,7 +4,7 @@ public class test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Hi, welcome everyone to my program. My name is Kushal Deep Singh and my student ID is 100851887.");
 	}
 
 }
